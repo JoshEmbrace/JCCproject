ee7cd43690b09887a8db3890bb2d48d553171146
JHTA <JHTA@JHTA-PC>
2019-06-19 12:41:06 +0900

    더보기 진행중

--- src/com/jhtacybercampus/web/dao/CurriDao.java
+++ src/com/jhtacybercampus/web/dao/CurriDao.java
@@ -8,11 +8,14 @@ import com.jhtacybercampus.web.entity.Curri;
 public interface CurriDao {
 
 	List<Curri> getList() throws ClassNotFoundException, SQLException;
+	List<Curri> getList(int page) throws ClassNotFoundException, SQLException;
 
 	Curri get(int id) throws ClassNotFoundException, SQLException;
 	
 	int delete(int id) throws ClassNotFoundException, SQLException;
 	int update(Curri curri) throws ClassNotFoundException, SQLException;
 	int insert(Curri curri) throws ClassNotFoundException, SQLException;
+
+
 	
 }
