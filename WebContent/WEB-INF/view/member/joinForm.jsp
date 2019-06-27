a4579d757f97907352897f37b958f495b5e6e335
JHTA <JHTA@JHTA-PC>
2019-06-27 10:22:19 +0900

    6-27 회원가입 완료

--- WebContent/WEB-INF/view/member/joinForm.jsp
+++ WebContent/WEB-INF/view/member/joinForm.jsp
@@ -2,21 +2,20 @@
 	pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
-<html lang="en" style="font-size: 10px">
+<html>
 
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
-	<link rel="stylesheet" href="../layout/layout.css">
 	<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!-- humcim -->
+	<!-- humchim -->
 	   <link rel="stylesheet" href="../css/member/join.css">
 
 	<!--  <link rel="stylesheet" type="text/css" href="../css/style.css"> -->
-	<script src="../js/member/join.js"></script>
+	<script src="../js/member/join.js?ver=1"></script>
 	<title>Sign up</title>
 </head>
 
--- WebContent/js/member/join.js
+++ WebContent/js/member/join.js
@@ -2,7 +2,7 @@ window.addEventListener("load",function(){
     var section = document.querySelector("#join");
     
     var id = section.querySelector("input[name=id]");
-    var checkid = section.querySelector("input[name=check-id]");
+    //var checkid = section.querySelector("input[name=check-id]");
     var pwd = section.querySelector("input[name=password]");
     var check = section.querySelector(".check-pwd");
     var pwd2 = section.querySelector("input[name=password1]");
@@ -13,23 +13,28 @@ window.addEventListener("load",function(){
 
     pwd.oninput = function(){
         if(pwd.value.includes(" ")){
-            check.innerText = "공백은 포함될 수 없습니다.";
-            check.style.color = "red";
+            //check.innerText = "공백은 포함될 수 없습니다.";
+            //check.style.color = "red";
+            check.value="not";
+            pwd.style.border = "2px solid orangered";
             
         }else{
-            check.innerText = "";
+            //check.innerText = "";
             check.value="ok";
+            pwd.style.border = "2px solid green";
         }
     }
     pwd2.oninput = function(){
         if(pwd.value == pwd2.value){
-            //pwdcheck.innerText = "일치합니다."
-            //pwdcheck.style.color = "green";
+            // pwdcheck.innerText = "일치합니다."
+            // pwdcheck.style.color = "green";
+            pwd.style.border = "2px solid green";
             pwd2.style.border = "2px solid green";
             pwdcheck.value="ok";
         }else{
             //pwdcheck.innerText = "불일치합니다."
             //pwdcheck.style.color = "red";
+
             pwd2.style.border = "2px solid orangered";
             pwdcheck.value="not";
         }
@@ -65,9 +70,24 @@ window.addEventListener("load",function(){
 
     var exist = section.querySelector(".check-id");
 
-    checkid.onchange = function(){
+    // id.onblur = function(){
+    //     alert("onblur");
+    //     id.style.border = "2px solid rgba(0, 0, 0, 0.02)";
+    //     id.style["background-image"] = 'url("/semi-JCC/images/guest/check-button1.png")';
+    // };
+
+    id.onchange = function(){
         var userid = id.value;
-        
+        if(userid == "" || userid == null || userid.includes(" ")){
+            alert("공백 는(은) 사용할 수 없습니다.");
+            // exist.innerText = "사용할 수 없습니다.";
+            // exist.style.color = "red";
+            id.style.border = "2px solid orangered";
+            id.style["background-image"] = 'url("/semi-JCC/images/guest/check-red.png")';
+            exist.value="not";
+            return;
+        }
+
 
         var request = new XMLHttpRequest();
         request.open("GET","join-check?id="+userid,false);
@@ -79,6 +99,7 @@ window.addEventListener("load",function(){
             // exist.innerText = "사용할 수 없습니다.";
             // exist.style.color = "red";
             id.style.border = "2px solid orangered";
+            id.style["background-image"] = 'url("/semi-JCC/images/guest/check-red.png")';
             exist.value="not";
             return;
         }else{
@@ -86,6 +107,7 @@ window.addEventListener("load",function(){
             //exist.innerText = "사용 가능 합니다.";
             //exist.style.color = "green";
             id.style.border = "2px solid green";
+            id.style["background-image"] = 'url("/semi-JCC/images/guest/check-green.png")';
             exist.value="ok";
         }
         
